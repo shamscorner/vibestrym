58b21f51f912b3bb33137de587bac58c9f62fd8a
Shamim <hossains159@gmail.com>
2025-07-03 16:28:31 +0600

    feat(graphql): add GraphQL configuration and development environment utility
    - Implemented `getGraphQLConfig` function to configure Apollo GraphQL settings.
    - Added playground support based on the environment.
    - Introduced `isDev` utility to determine if the application is running in development mode.
    - Configured environment variables using dotenv for better management.

---
+++ src/core/config/graphql.config.ts
@@ -0,0 +1,18 @@
+import type { ApolloDriverConfig } from '@nestjs/apollo'
+import { ConfigService } from '@nestjs/config'
+import { join } from 'path'
+
+import { isDev } from '@/src/shared/utils/is-dev.util'
+
+export function getGraphQLConfig(
+	configService: ConfigService
+): ApolloDriverConfig {
+	return {
+		playground: isDev(configService),
+		path: configService.getOrThrow<string>('GRAPHQL_PREFIX'),
+		autoSchemaFile: join(process.cwd(), 'src/core/graphql/schema.gql'),
+		sortSchema: true,
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		context: ({ req, res }) => ({ req, res })
+	}
+}
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,27 @@
+import { ApolloDriver } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
 
+import { IS_DEV_ENV } from '../shared/utils/is-dev.util'
+
+import { getGraphQLConfig } from './config/graphql.config'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-	imports: [PrismaModule],
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		}),
+		GraphQLModule.forRootAsync({
+			driver: ApolloDriver,
+			imports: [ConfigModule],
+			useFactory: getGraphQLConfig,
+			inject: [ConfigService]
+		}),
+		PrismaModule
+	],
 	controllers: [],
 	providers: []
 })
