2dfd86a35b74cd578c3a74f83869471f9ed8b891
Shamim <hossains159@gmail.com>
2025-07-24 03:45:27 +0600

    fix: reduce focus ring size for various UI components

--- frontend/src/components/ui/common/input-otp.tsx
+++ frontend/src/components/ui/common/input-otp.tsx
@@ -48,7 +48,7 @@ function InputOTPSlot({
   return (
     <div
       className={cn(
-        'relative flex h-16 w-auto flex-1 items-center justify-center border-input border-y border-r font-semibold text-xl shadow-xs outline-none transition-all first:rounded-l-md first:border-l last:rounded-r-md aria-invalid:border-destructive data-[active=true]:z-10 data-[active=true]:border-ring data-[active=true]:ring-[3px] data-[active=true]:ring-ring/50 data-[active=true]:aria-invalid:border-destructive data-[active=true]:aria-invalid:ring-destructive/20 dark:bg-input/30 dark:data-[active=true]:aria-invalid:ring-destructive/40',
+        'relative flex h-16 w-auto flex-1 items-center justify-center border-input border-y border-r font-semibold text-xl shadow-xs outline-none transition-all first:rounded-l-md first:border-l last:rounded-r-md aria-invalid:border-destructive data-[active=true]:z-10 data-[active=true]:border-ring data-[active=true]:ring-[1px] data-[active=true]:ring-ring/50 data-[active=true]:aria-invalid:border-destructive data-[active=true]:aria-invalid:ring-destructive/20 dark:bg-input/30 dark:data-[active=true]:aria-invalid:ring-destructive/40',
         className
       )}
       data-active={isActive}
