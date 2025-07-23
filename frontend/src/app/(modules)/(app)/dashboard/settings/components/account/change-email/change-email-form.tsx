75e2f6bc470f8911be79e30a07591cb28582d8c0
Shamim <hossains159@gmail.com>
2025-07-23 21:51:29 +0600

    feat: add change email form

---
+++ frontend/src/app/(modules)/(app)/dashboard/settings/components/account/change-email/change-email-form.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTranslations } from 'next-intl';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { useCurrentAccount } from '@/app/(modules)/(auth)/hooks/current-account';
+import { Button } from '@/components/ui/common/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/common/form';
+import { Input } from '@/components/ui/common/input';
+import { Skeleton } from '@/components/ui/common/skeleton';
+import { FormWrapper } from '@/components/ui/custom/form-wrapper';
+import { useChangeEmailMutation } from '@/graphql/_generated/output';
+import {
+  type ChangeEmailSchema,
+  changeEmailSchema,
+} from './change-email.schema';
+
+export function ChangeEmailForm() {
+  const t = useTranslations('dashboard.settings.account.email');
+
+  const { user, isLoadingProfile, refetch } = useCurrentAccount();
+
+  const form = useForm<ChangeEmailSchema>({
+    resolver: zodResolver(changeEmailSchema),
+    values: {
+      email: user?.email ?? '',
+    },
+  });
+
+  const [update, { loading: isLoadingUpdate }] = useChangeEmailMutation({
+    onCompleted() {
+      refetch();
+      toast.success(t('successMessage'));
+    },
+    onError() {
+      toast.error(t('errorMessage'));
+    },
+  });
+
+  const { isValid, isDirty } = form.formState;
+
+  function onSubmit(data: ChangeEmailSchema) {
+    update({ variables: { data } });
+  }
+
+  return isLoadingProfile ? (
+    <ChangeEmailFormSkeleton />
+  ) : (
+    <FormWrapper heading={t('heading')}>
+      <Form {...form}>
+        <form className="grid gap-y-3" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="px-5">
+                <FormLabel>{t('label')}</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoadingUpdate}
+                    placeholder="john.doe@example.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>{t('description')}</FormDescription>
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end px-5">
+            <Button disabled={!(isValid && isDirty) || isLoadingUpdate}>
+              {t('submitButton')}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </FormWrapper>
+  );
+}
+
+export function ChangeEmailFormSkeleton() {
+  return <Skeleton className="h-64 w-full" />;
+}
--- frontend/src/libs/i18n/request.ts
+++ frontend/src/libs/i18n/request.ts
@@ -1,12 +1,12 @@
 import { getRequestConfig } from 'next-intl/server';
-
+import type { Language } from './config';
 import { getCurrentLanguage } from './language';
 
 export default getRequestConfig(async () => {
   const language = await getCurrentLanguage();
 
   return {
-    locale: language,
+    locale: language as Language,
     messages: (await import(`../../../public/languages/${language}.json`))
       .default,
   };
