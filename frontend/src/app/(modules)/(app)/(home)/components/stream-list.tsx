104a120cc30e83dc18bb20d0c52db9191743393d
Shamim <hossains159@gmail.com>
2025-07-25 22:06:43 +0600

    feat: add homepage

---
+++ frontend/src/app/(modules)/(app)/(home)/components/stream-list.tsx
@@ -0,0 +1,26 @@
+import { EmptyState } from '@/components/ui/custom/empty-state'
+import { Heading } from '@/components/ui/custom/heading'
+
+import type { FindRandomStreamsQuery } from '@/graphql/_generated/output'
+
+import { StreamCard } from './stream-card'
+
+interface StreamsListProps {
+  heading?: string
+  streams: FindRandomStreamsQuery['findRandomStreams']
+}
+
+export function StreamsList({ heading, streams }: StreamsListProps) {
+  return streams.length ? (
+    <>
+      {heading && <Heading title={heading} />}
+      <div className='mt-6 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {streams.map((stream, index) => (
+          <StreamCard key={index} stream={stream} />
+        ))}
+      </div>
+    </>
+  ) : (
+    <EmptyState />
+  )
+}
---
+++ frontend/src/app/(modules)/(app)/(home)/page.tsx
@@ -0,0 +1,90 @@
+import { captureException } from '@sentry/nextjs';
+import { getTranslations } from 'next-intl/server';
+import { SERVER_URL } from '@/constants/url.constants';
+import {
+  FindRandomCategoriesDocument,
+  type FindRandomCategoriesQuery,
+  FindRandomStreamsDocument,
+  type FindRandomStreamsQuery,
+} from '@/graphql/_generated/output';
+import { CategoriesList } from './components/categories-list';
+import { StreamsList } from './components/stream-list';
+
+async function findRandomStreams() {
+  try {
+    const query = FindRandomStreamsDocument.loc?.source.body;
+
+    const response = await fetch(SERVER_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+      next: {
+        revalidate: 30,
+      },
+    });
+
+    const data = await response.json();
+
+    return {
+      streams: data.data
+        .findRandomStreams as FindRandomStreamsQuery['findRandomStreams'],
+    };
+  } catch (error) {
+    captureException(error, {
+      extra: {
+        feature: 'home',
+      },
+    });
+    throw new Error('Error fetching streams');
+  }
+}
+
+async function findRandomCategories() {
+  try {
+    const query = FindRandomCategoriesDocument.loc?.source.body;
+
+    const response = await fetch(SERVER_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+      next: {
+        revalidate: 30,
+      },
+    });
+
+    const data = await response.json();
+
+    return {
+      categories: data.data
+        .findRandomCategories as FindRandomCategoriesQuery['findRandomCategories'],
+    };
+  } catch (error) {
+    captureException(error, {
+      extra: {
+        feature: 'home',
+      },
+    });
+    throw new Error('Error fetching categories');
+  }
+}
+
+export default async function HomePage() {
+  const t = await getTranslations('home');
+
+  const { streams } = await findRandomStreams();
+  const { categories } = await findRandomCategories();
+
+  return (
+    <div className="flex flex-col gap-y-10">
+      <StreamsList heading={t('streamsHeading')} streams={streams} />
+      <CategoriesList
+        categories={categories}
+        heading={t('categoriesHeading')}
+      />
+    </div>
+  );
+}
