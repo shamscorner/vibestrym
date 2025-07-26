075430cdca8a8e453c5efe3e07ee5be86e212dc8
Shamim <hossains159@gmail.com>
2025-07-26 19:16:52 +0600

    feat: add streams page

---
+++ frontend/src/app/(modules)/(app)/streams/components/stream-content.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+import { Heading } from '@/components/ui/custom/heading';
+
+import {
+  type FindAllStreamsQuery,
+  useFindAllStreamsQuery,
+} from '@/graphql/_generated/output';
+
+import { StreamCardSkeleton } from './stream-card';
+import { StreamsList } from './stream-list';
+
+interface StreamsContentProps {
+  streams: FindAllStreamsQuery['findAllStreams'];
+}
+
+export function StreamsContent({ streams }: StreamsContentProps) {
+  const t = useTranslations('streams');
+
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get('searchTerm');
+
+  const [streamList, setStreamList] = useState<
+    FindAllStreamsQuery['findAllStreams']
+  >(streams ?? []);
+  const [hasMore, setHasMore] = useState(true);
+
+  const { data, fetchMore } = useFindAllStreamsQuery({
+    variables: {
+      filters: {
+        searchTerm,
+        take: 12,
+        skip: 0,
+      },
+    },
+    fetchPolicy: 'network-only',
+  });
+
+  useEffect(() => {
+    if (data?.findAllStreams) {
+      setStreamList(data.findAllStreams);
+      setHasMore(data.findAllStreams.length === 12);
+    }
+  }, [data]);
+
+  function fetchMoreStreams() {
+    if (!hasMore) return;
+
+    setTimeout(async () => {
+      const { data: newData } = await fetchMore({
+        variables: {
+          filters: {
+            searchTerm,
+            take: 12,
+            skip: streamList.length,
+          },
+        },
+      });
+
+      if (newData.findAllStreams.length) {
+        setStreamList((prev) => [...prev, ...newData.findAllStreams]);
+      } else {
+        setHasMore(false);
+      }
+    }, 400);
+  }
+
+  return (
+    <>
+      <Heading
+        title={
+          searchTerm ? `${t('searchHeading')} "${searchTerm}"` : t('heading')
+        }
+      />
+      <InfiniteScroll
+        dataLength={streamList.length}
+        hasMore={hasMore}
+        loader={
+          <div className="mt-6 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {Array.from({ length: 12 }).map((_, index) => (
+              <StreamCardSkeleton key={index} />
+            ))}
+          </div>
+        }
+        next={fetchMoreStreams}
+      >
+        <StreamsList streams={streamList} />
+      </InfiniteScroll>
+    </>
+  );
+}
---
+++ frontend/src/app/(modules)/(app)/streams/page.tsx
@@ -0,0 +1,64 @@
+import { captureException } from '@sentry/nextjs';
+import type { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
+import { SERVER_URL } from '@/constants/url.constants';
+import {
+  FindAllStreamsDocument,
+  type FindAllStreamsQuery,
+} from '@/graphql/_generated/output';
+import { StreamsContent } from './components/stream-content';
+
+async function findAllStreams() {
+  try {
+    const query = FindAllStreamsDocument.loc?.source.body;
+    const variables = {
+      filters: {},
+    };
+
+    const response = await fetch(SERVER_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query, variables }),
+      next: {
+        revalidate: 30,
+      },
+    });
+
+    const data = await response.json();
+
+    return {
+      streams: data.data
+        .findAllStreams as FindAllStreamsQuery['findAllStreams'],
+    };
+  } catch (error) {
+    captureException(error, {
+      extra: {
+        location: 'findAllStreams',
+        serverUrl: SERVER_URL,
+      },
+    });
+    throw new Error('Failed to fetch streams');
+  }
+}
+
+export async function generateMetadata(props: {
+  searchParams: Promise<{ searchTerm: string }>;
+}): Promise<Metadata> {
+  const t = await getTranslations('streams');
+
+  const searchParams = await props.searchParams;
+
+  return {
+    title: searchParams.searchTerm
+      ? `${t('searchHeading')} "${searchParams.searchTerm}"`
+      : t('heading'),
+  };
+}
+
+export default async function StreamsPage() {
+  const { streams } = await findAllStreams();
+
+  return <StreamsContent streams={streams} />;
+}
