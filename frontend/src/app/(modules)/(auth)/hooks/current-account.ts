63a2cdc6fac09093b4002d830872927535c05157
Shamim <hossains159@gmail.com>
2025-08-01 15:18:03 +0600

    refactor: clean up error handling and remove unused router import

--- frontend/src/app/(modules)/(auth)/hooks/current-account.ts
+++ frontend/src/app/(modules)/(auth)/hooks/current-account.ts
@@ -1,29 +1,31 @@
-import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import { useFindProfileQuery } from '@/graphql/_generated/output';
+import {
+  useClearSessionCookieMutation,
+  useFindProfileQuery,
+} from '@/graphql/_generated/output';
 import { useAuth } from '.';
 
 export function useCurrentAccount() {
-  const router = useRouter();
   const { isAuthenticated, exit } = useAuth();
 
   const { data, loading, refetch, error } = useFindProfileQuery({
     skip: !isAuthenticated,
   });
 
+  const [clear] = useClearSessionCookieMutation();
+
   useEffect(() => {
-    if (error && process.env.NODE_ENV === 'development') {
-      // biome-ignore lint/suspicious/noConsole: <only for development>
-      console.error('Error fetching current account:', error);
-      return;
-    }
+    // if (error && process.env.NODE_ENV === 'development') {
+    //   // biome-ignore lint/suspicious/noConsole: <only for development>
+    //   console.error('Error fetching current account:', error);
+    //   return;
+    // }
     if (!error) return;
     if (isAuthenticated) {
-      router.push('/logout');
-      return;
+      clear();
     }
     exit();
-  }, [isAuthenticated, exit, error, router.push]);
+  }, [isAuthenticated, exit, error, clear]);
 
   return {
     user: data?.findProfile,
