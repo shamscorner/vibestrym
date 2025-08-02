7b43b7d9262a6af234a37d51f2a9f1ce895c98de
Shamim <hossains159@gmail.com>
2025-08-02 11:45:10 +0600

    refactor: update Logo component to use SVG and remove image import

--- frontend/src/app/(modules)/(app)/components/header/header.tsx
+++ frontend/src/app/(modules)/(app)/components/header/header.tsx
@@ -5,7 +5,7 @@ import { HeaderMenu } from './header-menu';
 export function Header() {
   return (
     <header className="flex h-full items-center gap-x-4 border-border border-b bg-card p-4">
-      <Logo height={58} width={120} />
+      <Logo />
       <Search />
       <HeaderMenu />
     </header>
