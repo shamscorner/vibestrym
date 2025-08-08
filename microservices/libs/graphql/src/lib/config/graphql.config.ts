0518fd980248ee2dfbbb3a9c1cbc3d444b08d3c0
Shamim <hossains159@gmail.com>
2025-08-08 14:55:29 +0600

    feat(graphql): update GraphQL configuration to enable graphiql in development and disable playground

--- microservices/libs/graphql/src/lib/config/graphql.config.ts
+++ microservices/libs/graphql/src/lib/config/graphql.config.ts
@@ -1,16 +1,22 @@
 import { GqlConfig } from '.';
+import { CoreAppConfig } from '@microservices/core';
 import type { ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigService } from '@nestjs/config';
 
 export function getGraphQLConfig(
 	configService: ConfigService
 ): ApolloDriverConfig {
+	const isDevelopment =
+		configService.get<CoreAppConfig['env']['type']>('NODE_ENV') ===
+		'development';
+
 	const gqlConfig = configService.get<GqlConfig['graphql']>('graphql');
 
 	const graphqlPath = gqlConfig?.prefix || '/graphql';
 
 	return {
-		playground: true,
+		graphiql: isDevelopment,
+		playground: false,
 		path: graphqlPath,
 		autoSchemaFile: true,
 		sortSchema: true,
