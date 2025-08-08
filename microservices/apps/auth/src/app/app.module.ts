4aba4ae1c3a02b1111bd7f81a369faeb17ae8c4b
Shamim <hossains159@gmail.com>
2025-08-08 06:19:21 +0600

    feat(config): integrate ConfigModule for global configuration management

--- microservices/apps/auth/src/app/app.module.ts
+++ microservices/apps/auth/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-	imports: [PrismaModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		PrismaModule
+	],
 	controllers: [],
 	providers: []
 })
--- microservices/apps/auth/src/main.ts
+++ microservices/apps/auth/src/main.ts
@@ -1,12 +1,17 @@
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	const port = process.env.APPLICATION_PORT || 3000;
+
+	const config = app.get(ConfigService);
+
+	const port = config.get('APPLICATION_PORT') || 3000;
 	await app.listen(port);
+
 	Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
